ae03c556fd89755f0363cc864fd4a0169c7559b4
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-09-29 20:37:10 +0200

    finished slide schema

---
+++ studio/schemas/slide.js
@@ -0,0 +1,42 @@
+import { BiSlideshow } from 'react-icons/bi';
+
+export default {
+  title: 'Slide',
+  name: 'slide',
+  type: 'object',
+  icon: BiSlideshow,
+  __experimental_actions: ['update', 'publish', 'create'],
+  fields: [
+    {
+      title: 'Slide Text',
+      name: 'text',
+      type: 'object',
+      fields: [
+        {
+          title: 'Heading',
+          name: 'heading',
+          type: 'string',
+        },
+        {
+          title: 'Subheading',
+          name: 'subheading',
+          type: 'string',
+        }
+      ],
+      options: {
+        i18n: true,
+      },
+    },
+    {
+      title: 'Background Image',
+      name: 'backgroundImage',
+      type: 'image',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'text.de.heading',
+      media: 'backgroundImage',
+    },
+  },
+}

207ef9f1aec6e110ac22e081151c1fd2df9b25a1
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-10-12 19:12:42 +0200

    added text and list content types

---
+++ components/content/Emphasis.vue
@@ -0,0 +1,11 @@
+<template>
+  <i>
+    <slot></slot>
+  </i>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ components/content/List.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul v-if="listItem === 'number'">
+    <slot></slot>
+  </ul>
+  <ol v-else>
+    <slot></slot>
+  </ol>
+</template>
+
+<script>
+  export default {
+    props: ['listItem'],
+  }
+</script>
+
+<style>
+  li {
+    color: #000000;
+    font-size: 18px;
+    line-height: 30px;
+  }
+  ul, ol {
+    margin-bottom: 0;
+  }
+</style>
